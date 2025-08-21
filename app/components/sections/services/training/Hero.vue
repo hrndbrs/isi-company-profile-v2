a8ebf706e5c6e199b4f50476559f3bd95f9f82bd
Hernando <hrndbrs@gmail.com>
2025-08-21 14:55:16 +0700

    created hero section for /services/training route

---
+++ app/components/sections/services/training/Hero.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { SplitText } from "gsap/all";
+
+useGSAP(
+  (gsap) => {
+    const titleSplit = SplitText.create("p, #trainingHeroSectionTitle", {
+      type: "lines",
+    });
+
+    gsap.from(titleSplit.lines, {
+      opacity: 0,
+      translateX: "-10%",
+      stagger: 0.15,
+      scrollTrigger: {
+        trigger: titleSplit.lines,
+        start: "top bottom",
+      },
+    });
+  },
+  {
+    scope: "#trainingHeroSection",
+  },
+);
+</script>
+
+<template>
+  <SectionWrapper
+    id="trainingHeroSection"
+    class="max-md:pb-14 py-64 text-brand-200"
+    aria-label="Training Service Hero"
+    :inner-container-props="{
+      class: 'gap-3',
+    }"
+  >
+    <p class="text-h3 font-bold">Let’s Become Better!</p>
+    <h1 id="trainingHeroSectionTitle" class="text-h1">
+      Soft-skill Training <em class="font-normal">Sessions</em>
+    </h1>
+    <p class="text-xl">
+      Boost your communication, leadership, teamwork, and emotional intelligence
+      through our interactive workshops designed for real-world application.
+    </p>
+    <Button class="self-start"> Start Here </Button>
+  </SectionWrapper>
+</template>
---
+++ app/pages/services/training.vue
@@ -0,0 +1,6 @@
+<template>
+  <ServicesTrainingHero />
+  <SharedTestimonial />
+  <SharedCTA />
+  <SharedFAQ />
+</template>
