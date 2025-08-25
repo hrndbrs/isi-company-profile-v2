dad24eeef3d85127770d5608cf5d14196e772aa5
Hernando <hrndbrs@gmail.com>
2025-08-25 22:11:40 +0700

    created custom lottie component
    lottie shouldnt be played when not in view for performance

---
+++ app/components/ButtonWithWave.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const buttonRef = useTemplateRef<HTMLButtonElement>("buttonRef");
+const isHovered = useElementHover(buttonRef);
+const size = 22;
+</script>
+
+<template>
+  <Button ref="buttonRef">
+    <slot />
+    <Lottie
+      :pause-animation="!isHovered"
+      animation-link="/assets/lotties/wave.json"
+      class="rounded-full !bg-neutral-50"
+      :width="size"
+      :height="size"
+    />
+  </Button>
+</template>
---
+++ app/components/Lottie.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Vue3Lottie } from "vue3-lottie";
+
+const containerRef = useTemplateRef<HTMLDivElement>("containerRef");
+const isVisible = useElementVisibility(containerRef);
+</script>
+
+<template>
+  <ClientOnly>
+    <Vue3Lottie
+      ref="containerRef"
+      class="inline-block"
+      v-bind="{
+        pauseAnimation: !isVisible,
+        noMargin: true,
+        ...$attrs,
+      }"
+    />
+  </ClientOnly>
+</template>
